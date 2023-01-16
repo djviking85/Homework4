8178243d454aba98287e8ffe9fee7e3c16cf64c5
Viking <djviking@yandex.ru>
2023-01-16 22:56:04 +0300

    4 дз 4.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int manYears = 18;
+        if (manYears < 18) {
+            System.out.println( "Если возраст человека равен " + manYears + " годам, то он не достиг совершеннолетия, нужно немного подождать");
+        }
+        if (manYears >= 18) {
+            System.out.println( "Человеку " + manYears + " лет, он совершеннолетний!");
+        }
+        if (manYears < 18) {
+            System.out.println( " ты еще мал, тебе " + manYears + " лет!!");
+        } else {
+            System.out.println( "Да ты совершеннолетний ");}
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+
     }
 }
